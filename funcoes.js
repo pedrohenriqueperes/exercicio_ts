eb5000f14c5dbd2d3581590fb75f444b822768d5
pedrohenriqueperes <php741@protonmail.com>
2024-04-12 14:14:06 -0300

    first commit

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+"use strict";
+function greet(name) {
+    return "Olá " + name + "!";
+}
+// Exemplo de uso:
+const greeting = greet("Pedro");
+console.log(greeting); // Output: Olá Pedro!
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+// Exemplo de uso:
+const product = multiply(10, 5);
+console.log(product); // Output: 50
---
+++ funcoes.ts
@@ -0,0 +1,16 @@
+function greet(name: string): string {
+    return "Olá " + name + "!";
+}
+
+// Exemplo de uso:
+const greeting = greet("Pedro");
+console.log(greeting);  // Output: Olá Pedro!
+
+
+function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Exemplo de uso:
+const product = multiply(10, 5);
+console.log(product);  // Output: 50
